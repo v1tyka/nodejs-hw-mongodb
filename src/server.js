0eddac35e4c44bda1ea13d193bbc0eb0f55e755e
v1tyka <racinvictor@gmail.com>
2025-10-08 20:26:19 +0200

    commit 18

--- src/server.js
+++ src/server.js
@@ -1,30 +1,35 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import contactsRouter from './routers/contacts.js';
-import authRouter from './routers/auth.js';
+import router from './routers/index.js';
+import * as dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-
-export const setupServer = () => {
+dotenv.config();
+const PORT = process.env.PORT;
+function setupServer() {
   const app = express();
 
-  app.use(cors());
-  app.use(pino());
   app.use(express.json());
+  app.use(cors());
+  app.use(cookieParser());
+  app.use(router);
 
-  // ✅ Роути
-  app.use('/contacts', contactsRouter);
-  app.use('/auth', authRouter);
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    })
+  );
 
-  // ✅ Обробка помилок
   app.use(notFoundHandler);
   app.use(errorHandler);
 
-  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
+}
 
-  return app;
-};
+export default setupServer;
