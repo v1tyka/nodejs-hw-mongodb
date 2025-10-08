0f11ad26b3957ff2ea4857d395c7d5c0b411f90c
v1tyka <racinvictor@gmail.com>
2025-10-08 19:52:59 +0200

    commit 16

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { setupServer } from './server.js';
+import { setupServer } from '../src/server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
 await initMongoConnection();
